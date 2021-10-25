1c25e99e1ca83e4ed41aa38d2dc7c764ab3bc636
Mauricio Cruz <mauricio11111@gmail.com>
2021-10-24 23:45:05 -0300

    Incorporando Framework7

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+import {
+  Page,
+  Navbar,
+  BlockTitle,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Link,
+  List,
+  ListItem,
+} from 'framework7-react';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+        <Card>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        </Card>
+
     </div>
   );
 }
